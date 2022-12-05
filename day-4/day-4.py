84607d0c86d9d6181223b04c6aacefcdb95c6c68
César <cesarlpb@gmail.com>
2022-12-05 17:00:04 +0100

    Day 4 - Hasta completar secuencias numéricas de intervalos

--- day-4/day-4.py
+++ day-4/day-4.py
@@ -5,4 +5,27 @@
 # Iterar sobre el rango más corto -> verificar si todos sus números están contenidos en el otro
 # Aumentamos contador de incluídos
 # Retornamos contador
+#%% Leer inputo o test
+def leer_txt(archivo):
+    with open(archivo, 'r') as f:
+        input = f.read().splitlines()
+    return input
+#%% Reordenamos secuencias
 
+def completar_secuencias(lista):
+    nueva_lista = []
+    lim_inf = lista[0]
+    lim_sup = lista[1]
+    for i in range(int(lim_inf), int(lim_sup)+1):
+        nueva_lista.append(i)
+    return nueva_lista
+
+def reordenar_secuencias(linea):
+    linea = linea.split(',')
+    linea = [x.split('-') for x in linea]
+    print(linea[0])
+    izquierda  = completar_secuencias(linea[0])
+    derecha = completar_secuencias(linea[1])
+    print(izquierda, derecha)
+    
+reordenar_secuencias("2-4,6-8")
