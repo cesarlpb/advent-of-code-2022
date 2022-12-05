fcaeae9d17a940532a7ab123636dd41709714389
César <cesarlpb@gmail.com>
2022-12-05 12:27:55 +0100

    Day 3 - código inicial

---
+++ day-3/day-3.py
@@ -0,0 +1,7 @@
+# Leer txt
+# Separar en mitades
+# Iterar ambas mitades para determinar qué caracter se repite
+    # print -> el caracter que se repite
+# Calcular prioridad
+# Calcular sumas de prioridades
+
