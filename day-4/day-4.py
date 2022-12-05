ec848ac0c6f7d75c8d245368d5a838bc555b30d9
César <cesarlpb@gmail.com>
2022-12-05 16:23:16 +0100

    Day 4 - código inicial

---
+++ day-4/day-4.py
@@ -0,0 +1,8 @@
+# Day 4 - https://adventofcode.com/2022/day/4
+
+# X Leer txt
+# Crear rangos numéricos de los intervalos
+# Iterar sobre el rango más corto -> verificar si todos sus números están contenidos en el otro
+# Aumentamos contador de incluídos
+# Retornamos contador
+
