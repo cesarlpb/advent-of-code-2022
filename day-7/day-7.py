bfa21661287634a58b1798bb0d68957678628cd5
César <cesarlpb@gmail.com>
2022-12-09 14:40:55 +0100

    Day 7 - código inicial

---
+++ day-7/day-7.py
@@ -0,0 +1,24 @@
+# Estructura de datos
+    # Diccionario -> carpetas y archivos
+        # Lista -> archivos en carpeta
+        # tupla (int, str) -> cada archivo
+# my_dict: {
+# "/": {
+#   "dir a": {
+#       "dir e": {
+#           "i": 584
+#           }, 
+#       "f": 1212 
+#       "g": 2557, 
+#       "h.lst": 62596
+#      },
+#  "dir d": {
+#       "j": 4060174
+#       "d.log":  8033020,
+#       "d.ext": 5626152,
+#       "k": 7214296
+#    }
+# }
+# carpeta -> dict
+# archivo -> tupla (int, str)
+# Diagrama de los directorios y archivos
