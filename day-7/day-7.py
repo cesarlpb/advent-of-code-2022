8e10b48ce60b98194cd705e8b9ba71fc1602950a
César <cesarlpb@gmail.com>
2022-12-09 17:00:50 +0100

    Day 7 - Resolver problema con dir a

--- day-7/day-7.py
+++ day-7/day-7.py
@@ -30,6 +30,55 @@ def leer_txt(archivo):
         datos = f.read().splitlines()
     return datos
 datos_test = leer_txt('test-7.txt')
+#%% Definimos funciones auxiliares
+# Navegar a la key del elemento en tree
+def navegar_a_key(pwd):
+    # if tree.get(root) is None:
+    #     tree[root] = {}
+    el = tree.get(root)
+    print("el:", el)
+    idx = 1
+    while idx < len(pwd):
+        key = pwd[idx]
+        el = el.get(key)
+        idx += 1
+    return el
+# Registra que hemos cambiado a la carpeta superior
+def navegar_a_parent(pwd):
+    pwd.pop()
+
+# Registra que hemos cambiado a una carpeta inferior
+def navegar_a_child(pwd, nombre_dir):
+    el = navegar_a_key(pwd)
+    print("nombre_dir desde nav_child:", nombre_dir)
+    if el is None:
+        tree[nombre_dir] = {}
+    pwd.append(nombre_dir)
+
+# Guarda los archivos o carpetas hasta encontrar otro comando
+def guardar_archivos_o_dirs():
+    pass
+# Crea un diccionario
+def crear_dict_de_dir(pwd, nombre_dir):
+    el = tree.get(root)
+    # print("el:", el)
+    idx = 1
+    while idx < len(pwd):
+        key = pwd[idx]
+        print("key:", key, "el:", el)
+        temp = el.get(key)
+        if temp is None:
+            el[nombre_dir] = {}
+        print("el:", el)
+        idx += 1
+    else: 
+        pass
+# Guarda un archivo en el diccionario de su carpeta
+def guardar_archivo_en_dict(size, nombre_archivo):
+    # navegar a la posición actual en el dict
+    # guardar archivo como size: nombre_archivo
+    if navegar_a_key(pwd).get(nombre_archivo) is None:
+        navegar_a_key(pwd)[size] = nombre_archivo
 #%% 2. Interpretamos comandos y lista de dirs y archivos
     # Comandos: empieza la línea por $
     # Directorios: empieza la línea por dir
@@ -37,7 +86,6 @@ datos_test = leer_txt('test-7.txt')
 tree = {}
 root = "/"
 pwd = []
-dir_actual = ""
 def interpretar_comandos(datos):
     for linea in datos:
         if linea[0] == "$":
@@ -46,41 +94,33 @@ def interpretar_comandos(datos):
             if comando.startswith("cd"):
                 if comando.endswith(".."):
                     # lógica para ir a carpeta superior
-                    navegar_a_parent()
+                    navegar_a_parent(pwd)
                 else:
                     # lógica para ir a carpeta inferior
-                    navegar_a_child()
+                    nombre_dir = linea.split(" ", 2)[-1]
+                    navegar_a_child(pwd, nombre_dir)
             elif comando.startswith("ls"):
                 # tomar archivos o carpetas hasta encontrar otro comando
-                guardar_archivos_o_dirs()
+                # guardar_archivos_o_dirs()
+                pass
         elif linea.startswith("dir"):
             nombre_dir = linea.split(" ", 1)[1]
+            print(pwd, nombre_dir)
             crear_dict_de_dir(pwd, nombre_dir)
         elif linea.split(" ")[0].isdigit():
-            guardar_archivo_en_dict()
+            [size, nombre_archivo] = linea.split(" ")
+            guardar_archivo_en_dict(size, nombre_archivo)
 interpretar_comandos(datos_test)
-#%% Definimos funciones auxiliares
-# Registra que hemos cambiado a la carpeta superior
-def navegar_a_parent():
-    pass
-# Registra que hemos cambiado a una carpeta inferior
-def navegar_a_child():
-    pass
-# Guarda los archivos o carpetas hasta encontrar otro comando
-def guardar_archivos_o_dirs():
-    pass
-# Crea un diccionario
-def crear_dict_de_dir(pwd, nombre_dir):
-    el = tree.get(root)
-    idx = 1
-    while idx < len(pwd):
-        key = pwd[idx]
-        el = el.get(key)
-        idx += 1
-    if el.get(nombre_dir) is None:
-        el[nombre_dir] = {}
-    else: 
-        pass
-# Guarda un archivo en el diccionario de su carpeta
-def guardar_archivo_en_dict():
-    pass
+# %% Comprobamos SI el size es único en el input
+def comprobar_size_unico():
+    datos_input = leer_txt('input-7.txt')
+    lista_de_archivos = []
+    for linea in datos_input:
+        if linea.split(" ")[0].isdigit():
+            [size, nombre_archivo] = linea.split(" ")
+            lista_de_archivos.append((size, nombre_archivo))
+    new_set = set(lista_de_archivos)
+    print("lista completa:", len(lista_de_archivos))
+    print("set:", len(new_set))
+comprobar_size_unico() # 294 y 294 => SI es único el size. El nombre_archivo si se repite pero en combinación ambos datos son únicos
+# %% # Resolver el problema con dir a -> que no se está añadiendo bien al dict
