d7f6c04089bd674947b6bd94bf641edb0f03f91d
César <cesarlpb@gmail.com>
2022-12-12 12:02:30 +0100

    Solución Day 8 - parte 1
    Hemos creado config del debugger para archivo actual

--- day-8/day-8.py
+++ day-8/day-8.py
@@ -31,26 +31,26 @@ def es_arbol_visible(fila, columna, datos):
     arboles_fila = datos[fila]
     col = 0
     while col < len(arboles_fila) and (es_visible_izq or es_visible_der):
-        if col < columna and arboles_fila[col] > arbol and col in range(1, len(arboles_fila)-1):
+        if col < columna and arboles_fila[col] >= arbol:
             es_visible_izq = False
-        elif col > columna and arboles_fila[col] > arbol and col in range(1, len(arboles_fila)-1):
+        elif col > columna and arboles_fila[col] >= arbol:
             es_visible_der = False
             break
         col += 1
     # Arriba y abajo
     fil = 0
     while fil < len(datos) and (es_visible_arriba or es_visible_abajo):
-        if fil < fila and datos[fil][columna] > arbol and fil in range(1, len(datos)-1):
+        if fil < fila and datos[fil][columna] >= arbol:
             es_visible_arriba = False
-        elif fil > fila and datos[fil][columna] > arbol and fil in range(1, len(datos)-1):
+        elif fil > fila and datos[fil][columna] >= arbol:
             es_visible_abajo = False
             break
         fil += 1
-    return es_visible_arriba
+    return es_visible_arriba or es_visible_abajo or es_visible_izq or es_visible_der
 #%% Bucle de árboles interiores
     # Entre 1 y n-1 si usamos range
         # salir del bucle si no es visible
-def calc_interiores(datos):
+def calc_interiores(datos, print_arboles=False):
     arboles_visibles_interior = 0
     fila, columna = 1, 1
     es_visible = False
@@ -60,13 +60,48 @@ def calc_interiores(datos):
             es_visible = es_arbol_visible(fila, columna, datos)
             if es_visible:
                 arboles_visibles_interior += 1
-                print("V", end=" ")
+                if print_arboles:
+                    print("V", end=" ")
             else:
-                print("N", end=" ")
+                if print_arboles:
+                    print("N", end=" ")
             columna += 1
-        print("")
+        if print_arboles:
+            print("")
         fila += 1
     return arboles_visibles_interior
 print("Árboles visibles:", calc_interiores(datos_test))
+#%% Test - fila = 1, columna = 3
+fila, columna = 1, 3
+datos_test = ['30373', '25512', '65332', '33549', '35390']
+def es_arbol_visible_test(fila, columna, datos):
+    es_visible_izq, es_visible_der, es_visible_arriba, es_visible_abajo = True, True, True, True
+    arbol = datos[fila][columna]
+    # Izquierda y derecha
+    arboles_fila = datos[fila]
+    col = 0
+    while col < len(arboles_fila) and (es_visible_izq or es_visible_der):
+        if col < columna and arboles_fila[col] > arbol:
+            es_visible_izq = False
+        elif col > columna and arboles_fila[col] > arbol:
+            es_visible_der = False
+            break
+        col += 1
+    # Arriba y abajo
+    fil = 0
+    while fil < len(datos) and (es_visible_arriba or es_visible_abajo):
+        if fil < fila and datos[fil][columna] > arbol:
+            es_visible_arriba = False
+        elif fil > fila and datos[fil][columna] > arbol:
+            es_visible_abajo = False
+            break
+        fil += 1
+    return es_visible_arriba or es_visible_abajo or es_visible_izq or es_visible_der
+print("1, 3", es_arbol_visible_test(fila, columna, datos_test)) # False -> correcto
 #%% Retornamos total de visibles
-print(calc_borde() + calc_interiores()) # 16 + 5 = 21
+print("Total visibles test:", calc_borde(len(datos_test)) + calc_interiores(datos_test, True)) # 16 + 5 = 21
+
+#%% Total árboles visibles en input
+print("Total visibles input:", calc_borde(len(datos_input)) + calc_interiores(datos_input)) # más de 392
+
+# %%
